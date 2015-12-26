6fdf4e340ff7f1455875be8689abe6f17663d16c
izaque <izk_arcangelo@hotmail.com>
2015-12-26 12:45:25 -0200

    Definição do adapter responsável pela exibição dos nomes no ListView

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -15,6 +17,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Izaque", "ALine", "Taisa", "Abner"};
+
+        ListView  lista = (ListView) findViewById(R.id.lista);
+        //converte o array em view para que possa ser adicionado na listView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        lista.setAdapter(adapter);
     }
 
 }
