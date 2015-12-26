400edfb837394c877b62d0bff7920c7ea31789e6
izaque <izk_arcangelo@hotmail.com>
2015-12-26 13:44:55 -0200

    Personalização do botão adicionar aluno, utilizando um xml para definir a forma oval do botão.

---
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -0,0 +1,29 @@
+package br.com.alura.agenda;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button botaosalvar = (Button) findViewById(R.id.formulario_salvar);
+
+        botaosalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Formulário Salvo", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
