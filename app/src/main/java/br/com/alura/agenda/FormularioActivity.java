ad41abd68e3a950251b9ba7b291499fd80fdd83f
izaque <izk_arcangelo@hotmail.com>
2015-12-26 15:32:54 -0200

    Remoção do comportamento do botão salvar e adição do comportamento de salvar no item de menu.

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,16 +1,18 @@
 package br.com.alura.agenda;
 
-import android.content.Intent;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.LineNumberInputStream;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -19,6 +21,9 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
+
+
+        /* remoção do comportamento do botão salvar
         Button botaosalvar = (Button) findViewById(R.id.formulario_salvar);
 
         botaosalvar.setOnClickListener(new View.OnClickListener() {
@@ -26,15 +31,33 @@ public class FormularioActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Move do formulário para a lista de alunos.
                 Toast.makeText(FormularioActivity.this, "Aluno Salvo", Toast.LENGTH_SHORT).show();
-                /** Existe o problema de pilha do android, pois o código abaixo cria uma nova activity
+                *//** Existe o problema de pilha do android, pois o código abaixo cria uma nova activity
                  * e não utiliza a que já existe, sendo assim para obter o mesmo comportamento de voltar para
                  * a activity anterior também é possível utilizar o método finish();
-                 */
+                 *//*
                 //Intent lista = new Intent(FormularioActivity.this, ListaAlunosActivity.class);
-                //startActivity(lista);/*
+                //startActivity(lista);*//*
                 finish(); // encerra a activity e retorna para a anterior.
             }
-        });
+        });*/
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuPersonalizado = getMenuInflater();
+        menuPersonalizado.inflate(R.menu.menu_formulario_ok, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.menu.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Aluno Salvo", Toast.LENGTH_SHORT).show();
+                finish(); // encerra a activity e retorna para a anterior.
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
